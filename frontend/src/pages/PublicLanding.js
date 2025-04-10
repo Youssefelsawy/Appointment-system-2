b1850c8b9133e80acd8ec449df107bf9a7213eb1
Youssefelsawy <yosefelsawy406@gmail.com>
2025-04-10 20:46:24 +0200

    guest booking button is included.

--- frontend/src/pages/PublicLanding.js
+++ frontend/src/pages/PublicLanding.js
@@ -8,12 +8,15 @@ const PublicLanding = () => {
       <p className="lead mb-4">
         Book appointments with top healthcare professionals
       </p>
-      <div className="d-flex justify-content-center gap-3">
+      <div className="d-flex justify-content-center gap-3 flex-wrap">
         <Button as={Link} to="/login" variant="primary" size="lg">
           Login
         </Button>
         <Button as={Link} to="/register" variant="outline-primary" size="lg">
           Register
+        </Button>
+        <Button as={Link} to="/guest-booking" variant="success" size="lg">
+          Book as Guest
         </Button>
       </div>
     </Container>
