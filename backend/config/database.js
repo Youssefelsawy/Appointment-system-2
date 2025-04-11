f131f9ec7541147e7c5464341d1041d4277522af
Youssefelsawy <yosefelsawy406@gmail.com>
2025-04-11 03:45:12 +0200

    changes to deploy back on render

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,16 +1,30 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
+module.exports = {
+  development: {
+    dialect: "postgres",
     host: process.env.DB_HOST,
-    dialect: "mysql",
-    port: process.env.DB_PORT,
-    logging: false,
-  }
-);
-
-module.exports = sequelize;
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+  production: {
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
